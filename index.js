f2f4c502da64bfb1d83b2f170ff92b59a706fb55
agustin-ovejero <ovejeroagustin2@gmail.com>
2025-12-14 11:29:29 -0300

    json-en-js

---
+++ index.js
@@ -0,0 +1,13 @@
+const user = {
+  nombre: "braiam",
+  apellido: "nova",
+  edad: 20,
+  pasatiempos: ["programar", "fulvo"],
+  direcion: {
+    ciudada: "Buenos Aires",
+    calle: "Alberto williams"
+  },
+  estudiando: true,
+}
+console.log(user)
+console.log(JSON.stringify(user))
