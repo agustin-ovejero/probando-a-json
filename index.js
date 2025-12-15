dcb3d55b38301c5ddc9737366647c1772474d720
agustin-ovejero <ovejeroagustin2@gmail.com>
2025-12-15 15:54:59 -0300

    dando-unos-detalles-a-la-forma-de-recorrer-listas

--- index.js
+++ index.js
@@ -19,18 +19,22 @@ const user = {
 const amigos = [
   {"nombre": "agustin", "pronombre": "lechuga"},
   {"nombre": "celina", "pronombre": "vacelina"},
-  {"nombre": "daro", "pronombre": "daria"}
+  {"nombre": "daro", "pronombre": "daria"},
+  {"nombre": "sandra", "pronombre": "norma"},
 ]
 user.friends = amigos
 
-let output = ''
+let output = '' // variable que gurdamos la salida del for
 
+// i < amigos.length singinaca haste que i sea menor de la cantidad de la lista de amigos
+// i++ significa que va aumentando en cada bucle un uno
 for (let i = 0; i < amigos.length; i++) {
   output = output + `<li>${amigos[i].nombre}</li>`
 }
 //console.log(JSON.stringify(amigos))
 //console.log(user.saludo())
 //console.log(user.musica())
-//console.log(JSON.stringify(user))
-document.getElementById('people')
+console.log(user)
+console.log(JSON.stringify(user))
+//document.getElementById('people')
 document.getElementById('people').innerHTML = output
